49be1e6d67fd37b1d85211389b269874fcc88686
Pei Wen Tang <redjackfred@gmail.com>
2023-10-18 12:12:28 -0700

    Initial

--- src/MartianAnalyzer.java
+++ src/MartianAnalyzer.java
@@ -3,7 +3,7 @@ public class MartianAnalyzer {
 
     public static void main(String[] args) {
         GenomeSeriesAnalyzer gsa = new GenomeSeriesAnalyzer("test.txt");
-
+        // Run
         gsa.run();
     }
 
